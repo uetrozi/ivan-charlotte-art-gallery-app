c25f29f998f16561033550801aac11b50af8a6bf
ivannemeth <nemethivn@gmail.com>
2023-12-19 12:38:59 +0100

    has css style

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,11 +1,30 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  background-color: black;
+  color: white;
+  border-radius: 10px;
+  padding: 5px;
+  text-decoration: none;
+  &:hover {
+    color: coral;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 10px;
+  padding-bottom: 15px;
+`;
 
 export default function Navigation() {
   return (
-    <>
-      <Link href="/">Spotlight</Link>
-      <Link href="/art-pieces">Pieces</Link>
-      <Link href="/favourites">Favourites</Link>
-    </>
+    <Container>
+      <StyledLink href="/">Spotlight</StyledLink>
+      <StyledLink href="/art-pieces">Pieces</StyledLink>
+      <StyledLink href="/favourites">Favourites</StyledLink>
+    </Container>
   );
 }
